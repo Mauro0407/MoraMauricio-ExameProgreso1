dce1214254a0730eb0843459f43ac38e09e869b9
Mauro0407 <mauricio.mora.caicedo@udla.edu.ec>
2024-10-29 07:58:45 -0500

    Commit 1: Se agregaron etiquetas

--- MoraMauricio-ExameProgreso1/Models/Celular.cs
+++ MoraMauricio-ExameProgreso1/Models/Celular.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MoraMauricio_ExameProgreso1.Models
 {
@@ -6,9 +7,12 @@ namespace MoraMauricio_ExameProgreso1.Models
     {
         [Key]
         public int Id { get; set; }
+        [MaxLength(100)]
         public string Modelo { get; set; }
+        [MaxLength(50)]
         public Decimal Precio { get; set; }
-        public Boolean IdCelular { get; set; }
         public DateTime Año { get; set; }
+        [ForeignKey("IdCelular")]
+        public Boolean IdCelular { get; set; }
     }
 }
